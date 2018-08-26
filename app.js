0b47388fccec8515065e9f6e0ad83ce9ddedf14e
Ron Kagan <ronkagan@users.noreply.github.com>
2018-08-26 19:27:44 -0400

    create a logRequest function

--- app.js
+++ app.js
@@ -1,124 +1,47 @@
-const express = require('express');
-const app = express();
+console.log = function() {};
+const fs = require('fs');
+const expect = require('chai').expect;
+const Structured = require('structured');
 
-const PORT = process.env.PORT || 4001;
+describe('', function() {
+  const code = fs.readFileSync('app.js', 'utf8');
+  it('', function() {
+    const logMatcher = /app\.[\s\S]+?\([\s\S]+?logRequest\(([\s\S]+?)\)/g;
+    const codeMatches = code.match(logMatcher);
+    expect(codeMatches.length, 'Did you use logError at the beginning of every route callback?').to.equal(7);
 
-app.use(express.static('public'));
-
-const jellybeanBag = {
-  mystery: {
-    number: 4
-  },
-  lemon: {
-    number: 5
-  },
-  rootBeer: {
-    number: 25
-  },
-  cherry: {
-    number: 3
-  },
-  licorice: {
-    number: 1
-  }
-};
-
-// Add your logging function here:
-
-
-
-app.get('/beans/', (req, res, next) => {
-  console.log('GET Request Received');
-  res.send(jellybeanBag);
-  console.log('Response Sent');
-});
-
-app.post('/beans/', (req, res, next) => {
-  console.log('POST Request Received');
-  let queryData = '';
-  req.on('data', (data) => {
-    queryData += data;
-  });
-
-  req.on('end', () => {
-    const body = JSON.parse(queryData);
-    const beanName = body.name;
-    if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
-      return res.status(400).send('Bean with that name already exists!');
-    }
-    const numberOfBeans = Number(body.number) || 0;
-    jellybeanBag[beanName] = {
-      number: numberOfBeans
+    const shouldNotMatchStructGet = function() {
+      app.$method(_, (req, res, next) => {
+        console.log('GET Request Received');
+      });
     };
-    res.send(jellybeanBag[beanName]);
-    console.log('Response Sent');
+
+    const shouldNotMatchStructPut = function() {
+      app.$method(_, (req, res, next) => {
+        console.log('PUT Request Received');
+      });
+    };
+
+    const shouldNotMatchStructPost = function() {
+      app.$method(_, (req, res, next) => {
+        console.log('POST Request Received');
+      });
+    };
+
+    const shouldNotMatchStructDelete = function() {
+      app.$method(_, (req, res, next) => {
+        console.log('DELETE Request Received');
+      });
+    };
+
+    const shouldNotMatch = [
+      Structured.match(code, shouldNotMatchStructGet),
+      Structured.match(code, shouldNotMatchStructPut),
+      Structured.match(code, shouldNotMatchStructPost),
+      Structured.match(code, shouldNotMatchStructDelete),
+    ].every(response => response !== false);
+
+    expect(shouldNotMatch, 'Did you remove all Request logging console.logs?').to.be.false;
+
   });
-});
-
-app.get('/beans/:beanName', (req, res, next) => {
-  console.log('GET Request Received');
-  const beanName = req.params.beanName;
-  if (!jellybeanBag[beanName]) {
-    console.log('Response Sent');
-    return res.status(404).send('Bean with that name does not exist');
-  }
-  res.send(jellybeanBag[beanName]);
-  console.log('Response Sent');
-});
-
-
-app.post('/beans/:beanName/add', (req, res, next) => {
-  console.log('POST Request Received');
-  const beanName = req.params.beanName;
-  if (!jellybeanBag[beanName]) {
-    return res.status(404).send('Bean with that name does not exist');
-  }
-  let queryData = '';
-  req.on('data', (data) => {
-    queryData += data;
-  });
-
-  req.on('end', () => {
-    const numberOfBeans = Number(JSON.parse(queryData).number) || 0;
-    jellybeanBag[beanName].number += numberOfBeans;
-    res.send(jellybeanBag[beanName]);
-    console.log('Response Sent');
-  });
-});
-
-app.post('/beans/:beanName/remove', (req, res, next) => {
-  console.log('POST Request Received');
-  const beanName = req.params.beanName;
-  if (!jellybeanBag[beanName]) {
-    return res.status(404).send('Bean with that name does not exist');
-  }
-  let queryData = '';
-  req.on('data', (data) => {
-    queryData += data;
-  });
-
-  req.on('end', () => {
-    const numberOfBeans = Number(JSON.parse(queryData).number) || 0;
-    if (jellybeanBag[beanName].number < numberOfBeans) {
-      return res.status(400).send('Not enough beans in the jar to remove!');
-    }
-    jellybeanBag[beanName].number -= numberOfBeans;
-    res.send(jellybeanBag[beanName]);
-    console.log('Response Sent');
-  });
-});
-
-app.delete('/beans/:beanName', (req, res, next) => {
-  console.log('DELETE Request Received');
-  const beanName = req.params.beanName;
-  if (!jellybeanBag[beanName]) {
-    return res.status(404).send('Bean with that name does not exist');
-  }
-  jellybeanBag[beanName] = null;
-  res.status(204).send();
-  console.log('Response Sent');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
 });
